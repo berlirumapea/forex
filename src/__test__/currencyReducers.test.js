3dc64a51dbe72bddcc878ed544edb6472e180c70
edgeofuniverse <noone30696@gmail.com>
2019-01-11 13:04:22 +0700

    reducers tests

--- src/__test__/currencyReducers.test.js
+++ src/__test__/currencyReducers.test.js
@@ -1,17 +1,154 @@
-import { currencyReducers } from '../reducers/currencyReducers';
-import {} from '../actions/types';
-import { DefaultSymbols } from '../constants'
+import { currencyReducers } from "../reducers/currencyReducers";
+import {
+  FETCH_CURRENCY_SUCCESS,
+  ADD_CURRENCY,
+  REMOVE_CURRENCY,
+  USD_VAL_ONCHANGE
+} from "../actions/types";
+import { DefaultSymbols } from "../constants";
 
-const initialState = {
-	rates: [],
-	symbols: [...DefaultSymbols],
-	usd_value: 10.00
-}
+let initialState = {
+  rates: [],
+  symbols: [...DefaultSymbols],
+  usd_value: 10.0
+};
 
+describe("reducers", () => {
+  it("should return the initial state", () => {
+    expect(currencyReducers(undefined, {})).toEqual(initialState);
+  });
 
-describe('reducers', () => {
-	it("should return the initial state", () => {
-		expect(currencyReducers(undefined, {})).toEqual(initialState)
-	})
-	
-})
+  describe("FETCH_CURRENCY_SUCCESS", () => {
+    it("should return manipulated currency", () => {
+      let payloadFromAPI = {
+        rates: { GBP: 0.7839011704 },
+        base: "USD",
+        date: "2019-01-10"
+      };
+
+      let expectedResult = {
+        rates: [
+          {
+            name: "GBP",
+            desc: "British Pounds",
+            rateVal: 0.7839011704,
+            rateTotal: 0.7839011704 * initialState.usd_value,
+            show: true
+          }
+        ]
+      };
+
+      expect(
+        currencyReducers(
+          {},
+          {
+            type: FETCH_CURRENCY_SUCCESS,
+            payload: payloadFromAPI
+          }
+        )
+      ).toEqual(expectedResult);
+    });
+  });
+
+  describe("ADD_CURRENCY", () => {
+    it("should not update the show status to true if it's already true", () => {
+      let currentState = {
+        rates: [
+          {
+            name: "GBP",
+            show: true
+          }
+        ]
+      };
+      expect(
+        currencyReducers(currentState, { type: ADD_CURRENCY, payload: "GBP" })
+      ).toEqual(currentState);
+    });
+
+    it("should update show status in currencry to true if it false", () => {
+      let state = {
+        rates: [
+          {
+            name: "GBP",
+            show: true
+          },
+          {
+            name: "IDR",
+            show: false
+          }
+        ]
+      };
+
+      let expectedStateResult = {
+        rates: [
+          {
+            name: "GBP",
+            show: true
+          },
+          {
+            name: "IDR",
+            show: true
+          }
+        ]
+      };
+
+      expect(
+        currencyReducers(state, { type: ADD_CURRENCY, payload: "IDR" })
+      ).toEqual(expectedStateResult);
+    });
+  });
+
+  describe("REMOVE_CURRENCY", () => {
+    it("should remove or update currency's show status to false", () => {
+      let state = {
+        rates: [
+          {
+            name: "GBP",
+            show: true
+          }
+        ]
+      };
+
+      let expectedStateResult = {
+        rates: [
+          {
+            name: "GBP",
+            show: false
+          }
+        ]
+      };
+
+      expect(
+        currencyReducers(state, { type: REMOVE_CURRENCY, payload: "GBP" })
+      ).toEqual(expectedStateResult);
+    });
+  });
+
+  describe("USD_VAL_ONCHANGE", () => {
+    it("should calculate total rate value when usd value changed", () => {
+      let state = {
+        rates: [
+          {
+            name: "GBP",
+            rateVal: 0.7839011704,
+            rateTotal: 0.7839011704
+          }
+        ]
+      };
+
+      let expectedStateResult = {
+        rates: [
+          {
+            name: "GBP",
+            rateVal: 0.7839011704,
+            rateTotal: 0.7839011704 * 10.0
+          }
+        ]
+      };
+
+      expect(
+        currencyReducers(state, { type: USD_VAL_ONCHANGE, payload: 10.00 })
+      ).toEqual(expectedStateResult);
+    });
+  });
+});
--- src/reducers/currencyReducers.js
+++ src/reducers/currencyReducers.js
@@ -1,34 +1,79 @@
-import { ADD_CURRENCY, FETCH_CURRENCY_REQUEST, FETCH_CURRENCY_SUCCESS, FETCH_CURRENCY_FAILURE, REMOVE_CURRENCY, UPDATE_USD_VALUE } from "../actions/types";
-import { Currencies, DefaultSymbols } from '../constants'
+import {
+  ADD_CURRENCY,
+  FETCH_CURRENCY_SUCCESS,
+  REMOVE_CURRENCY,
+  USD_VAL_ONCHANGE
+} from "../actions/types";
+import { Currencies, DefaultSymbols } from "../constants";
 
 const initialState = {
-	rates: [],
-	symbols: [...DefaultSymbols],
-	usd_value: 10.00
-}
+  rates: [],
+  symbols: [...DefaultSymbols],
+  usd_value: 10.0
+};
 
 export const currencyReducers = (state = initialState, action) => {
-	switch(action.type){
-		case FETCH_CURRENCY_SUCCESS:
-			return {
-				...state,
-				rates: manipulatedRates(action.payload)
-			}
-		default:
-			return state
-	}
+  switch (action.type) {
+    case FETCH_CURRENCY_SUCCESS:
+      return {
+        ...state,
+        rates: manipulatedRates(action.payload)
+      };
+    case ADD_CURRENCY:
+      return {
+        ...state,
+        rates: updateCurrencyToTrue(state.rates, action.payload)
+      };
+    case REMOVE_CURRENCY:
+      return {
+        ...state,
+        rates: updateCurrencyToFalse(state.rates, action.payload)
+      };
+    case USD_VAL_ONCHANGE:
+      return {
+        ...state,
+        rates: state.rates.map(rate => ({
+					...rate,
+					rateTotal: rate.rateVal * action.payload
+				}))
+      };
+    default:
+      return state;
+  }
+};
+
+function updateCurrencyToFalse(rates, symbol) {
+  let mappedRates = rates.map(rate => {
+    if (rate.name === symbol) {
+      return { ...rate, show: false };
+    } else {
+      return rate;
+    }
+  });
+  return mappedRates;
 }
 
-export const manipulatedRates = (data) => {
-	const rates = Object.keys(data.rates).map(
-		rate =>
-			rate !== "USD" && {
-				name: rate,
-				desc: Currencies.find(curr => curr.key === rate).desc,
-				rateVal: data.rates[rate],
-				rateTotal: data.rates[rate] * initialState.usd_value
-			}
-	);
-	return rates;
+function updateCurrencyToTrue(rates, symbol) {
+  let mappedRates = rates.map(rate => {
+    if (rate.name === symbol && !rate.show) {
+      return { ...rate, show: true };
+    } else {
+      return rate;
+    }
+  });
+  return mappedRates;
 }
 
+function manipulatedRates(data) {
+  const rates = Object.keys(data.rates).map(
+    rate =>
+      rate !== "USD" && {
+        name: rate,
+        desc: Currencies.find(curr => curr.key === rate).desc,
+        rateVal: data.rates[rate],
+        rateTotal: data.rates[rate] * initialState.usd_value,
+        show: true
+      }
+  );
+  return rates;
+}
